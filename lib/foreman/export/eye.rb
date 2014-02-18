40a659f53fe07b05ea523e57c5e7377f82c3d0fa
Rich Grundy <rich@stinkyink.com>
2014-02-18 09:18:12 +0000

    Added exporter and default template.

--- lib/foreman/export/eye.rb
+++ lib/foreman/export/eye.rb
@@ -1,6 +1,28 @@
+require 'erb'
+require 'foreman/export'
+
 module Foreman
   module Export
-    class Eye
+    class Eye < Foreman::Export::Base
+
+      def export_template(name, file=nil, template_root=nil)
+        if file && template_root
+          old_export_template name, file, template_root
+        else
+          name_without_first = name.split("/")[1..-1].join("/")
+          matchers = []
+          matchers << File.join(options[:template], name_without_first) if options[:template]
+          matchers << File.expand_path("~/.foreman/templates/#{name}")
+          matchers << File.expand_path("../../../../data/export/#{name}", __FILE__)
+          File.read(matchers.detect { |m| File.exists?(m) })
+        end
+      end
+
+      def export
+        super
+        clean "#{location}/#{app}.eye"
+        write_template "eye/master.eye.erb", "#{app}.eye", binding
+      end
     end
   end
 end
